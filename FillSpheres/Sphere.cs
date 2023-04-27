a6d57d0b1a03f2a2c69fec06858bbc5d88e0111c
TomasCarvalho <2606tom@gmail.com>
2023-04-27 20:07:03 +0100

    Implement class Sphere

--- FillSpheres/Sphere.cs
+++ FillSpheres/Sphere.cs
@@ -7,6 +7,30 @@ namespace FillSpheres
 {
     public class Sphere
     {
-        
+        private Color color;
+        private float radius;
+        private int throwCount;
+
+        public Sphere(Color color, float radius)
+        {
+            this.color = color;
+            this.radius = radius;
+            throwCount = 0;
+        }
+
+        public int GetTimesThrown() => throwCount;
+
+        public void Pop()
+        {
+            radius = 0f;
+        }
+
+        public void Throw()
+        {
+            if (radius>0f)
+            {
+                throwCount++;
+            }
+        }
     }
 }
