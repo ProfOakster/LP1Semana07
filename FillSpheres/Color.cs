72c2878941e1c4134361ab1c111550f9b0510dfc
TomasCarvalho <2606tom@gmail.com>
2023-04-27 19:54:38 +0100

    Implement class Color

--- FillSpheres/Color.cs
+++ FillSpheres/Color.cs
@@ -7,6 +7,49 @@ namespace FillSpheres
 {
     public class Color
     {
-        
+        private byte red;
+        private byte green;
+
+        private byte blue;
+        private byte alpha;
+
+
+        public Color(byte red, byte green, byte blue, byte alpha)
+        {
+            this.red = red;
+            this.green = green;
+            this.blue = blue;
+            this.alpha = alpha;
+        }
+        public Color(byte red, byte green, byte blue)
+        {
+            this.red = red;
+            this.green = green;
+            this.blue = blue;
+            this.alpha = 255;
+        }
+        public byte GetRed() => red;
+        public byte GetGreen() => green;
+        public byte GetBlue() => blue;
+        public byte GetAlpha() => alpha;
+        public byte GetGrey() => (red+green+blue)/3;
+
+
+        public void SetRed(byte red)
+        {
+            this.red = red;
+        }
+        public void SetGreen(byte green)
+        {
+            this.green = green;
+        }
+        public void SetBlue(byte blue)
+        {
+            this.blue = blue;
+        }
+        public void SetAlpha(byte alpha)
+        {
+            this.alpha = alpha;
+        }
     }
 }
