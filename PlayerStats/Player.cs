9c26575476918167de0e11727ece3a2f49217cc1
TomasCarvalho <2606tom@gmail.com>
2023-04-28 12:00:21 +0100

    Implement project PlayerStats

--- PlayerStats/Player.cs
+++ PlayerStats/Player.cs
@@ -7,6 +7,47 @@ namespace PlayerStats
 {
     public class Player
     {
-        
+        private int playedGames;
+        private int wonGames;
+
+        private float highScore;
+        public float HighScore
+        {
+            get => highScore;
+            set
+            {
+                if (value>highScore)
+                    highScore = value;
+            }
+        }
+
+        public string Name { get; }
+
+        public float WinRate
+        {
+            get
+            {
+                if (playedGames==0)
+                    return 0f;
+                else
+                    return (wonGames / playedGames);
+            }
+        }
+
+        public Player (string name)
+        {
+            Name = name;
+            playedGames = 0;
+            wonGames = 0;
+            highScore = 0;
+        }
+
+        public void PlayGame(bool win)
+        {
+            playedGames += 1;
+            if (win)
+                wonGames += 1;
+        }
+
     }
 }
